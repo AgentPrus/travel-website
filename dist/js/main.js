cb38d831e405c6345399c3efc1f41461787c6a53
AgentPrus <nikitaprus98@gmail.com>
2020-05-14 09:25:01 +0300

    create basic project structure and style home page

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+// Sidebar Functions
+function openNav() {
+  document.getElementById("side-nav").style.width = "450px";
+}
+
+function closeNav() {
+  document.getElementById("side-nav").style.width = "0";
+}
+
+// TODO: Add smooth scroll
+// TODO: Add text-auto-typing
