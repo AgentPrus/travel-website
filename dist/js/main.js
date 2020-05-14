38870081e28ca965e4310c2f290d8a40e716f068
AgentPrus <nikitaprus98@gmail.com>
2020-05-14 11:59:07 +0300

    add jquery smooth scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,3 +6,30 @@ function openNav() {
 function closeNav() {
   document.getElementById("side-nav").style.width = "0";
 }
+
+$(document).ready(function () {
+  // Jquery Smooth Scroll
+  $(".btn-up, .btn-down").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      let hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
